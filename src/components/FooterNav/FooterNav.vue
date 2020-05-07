28f184fbdde49348c831bc6e8f4253130a637bd7
sun <chentao980225@163.com>
2020-05-07 17:26:42 +0800

    update

--- src/components/FooterNav/FooterNav.vue
+++ src/components/FooterNav/FooterNav.vue
@@ -1,22 +1,22 @@
 <template>
     <div id="footerNavContainer">
-			<div class="item" @click="$router.push('/home')">
+			<div class="item" :class="{active:$route.path==='/home'}" @click="getPath('/home')">
 				<i class="u-icon u-icon-tabBar-index"></i>
 				<span>首页</span>
 			</div>
-			<div class="item" @click="$router.push('/category')">
+			<div class="item" :class="{active:$route.path==='/category'}" @click="getPath('/category')">
 				<i class="u-icon u-icon-tabBar-cate"></i>
 				<span>分类</span>
 			</div>
-			<div class="item" @click="$router.push('/topic')">
+			<div class="item" :class="{active:$route.path==='/topic'}" @click="getPath('/topic')">
 				<i class="u-icon u-icon-tabBar-topic"></i>
 				<span>值得买</span>
 			</div>
-			<div class="item" @click="$router.push('/cart')">
+			<div class="item" :class="{active:$route.path==='/cart'}" @click="getPath('/cart')">
 				<i class="u-icon u-icon-tabBar-cart"></i>
 				<span>购物车</span>
 			</div>
-			<div class="item" @click="$router.push('/personal')">
+			<div class="item" :class="{active:$route.path==='/personal'}" @click="getPath('/personal')">
 				<i class="u-icon u-icon-tabBar-user"></i>
 				<span>个人</span>
 			</div>
@@ -25,7 +25,11 @@
 
 <script  type="text/ecmascript-6">
     export default {
-			
+			methods:{
+				getPath(path){
+					path!==this.$route.path && this.$router.push(path)
+				}
+			}
     };
 </script>
 
@@ -39,11 +43,28 @@
 		background-color white
 		display flex
 		.item
+			box-sizing border-box
 			width 20%
 			display flex
 			flex-direction column
 			align-items center
 			justify-content center
+			&.active
+				>.u-icon-tabBar-index
+					background-position 0 -155px
+				>.u-icon-tabBar-cate
+					background-position 0 -75px
+				>.u-icon-tabBar-topic
+					background-position 0 -240px
+				>.u-icon-tabBar-cart
+					background-position 0 -1px
+					margin-top 5px
+				>.u-icon-tabBar-cart+span
+					margin-top -2px
+				>.u-icon-tabBar-user
+					background-position 0 -320px
+				>span
+					color red
 			.u-icon
 				background-image url(https://yanxuan-static.nosdn.127.net/hxm/yanxuan-wap/p/20161201/style/img/sprites/tabBar-s43a0dc8a7d-de25ef8e19.png?imageView&type=webp) 
 				background-size 40px 400px	
