028e6c77d9047e8fb70236ff6b7d8a2a5311b0f2
sun <chentao980225@163.com>
2020-05-08 18:30:19 +0800

    carousel

--- src/components/HeaderNav/HeaderNav.vue
+++ src/components/HeaderNav/HeaderNav.vue
@@ -1,147 +1,244 @@
 <template>
 	<div id="headerContainer">
-		<div class="headerSearch">
-			<a href="https://m.you.163.com/"></a>
-			<div class="searchBox">
-				<i class="icon-search"></i>
-				<span>搜索商品, 共25372款好物</span>
-			</div>
-			<button class="login">登录</button>
-		</div>
-		<div class="headerNav">
-      <div class="inner">
-        <ul class="navList">
-          <li class="navItem active">
-            <a href="javascript:;">推荐</a>
-          </li>
-          <li class="navItem">
-            <a href="javascript:;">居家生活</a>
-          </li>
-          <li class="navItem">
-            <a href="javascript:;">服饰鞋包</a>
-          </li>
-          <li class="navItem">
-            <a href="javascript:;">美食酒水</a>
-          </li>
-          <li class="navItem">
-            <a href="javascript:;">个护清洁</a>
-          </li>
-          <li class="navItem">
-            <a href="javascript:;">母婴亲子</a>
-          </li>
-          <li class="navItem">
-            <a href="javascript:;">运动旅行</a>
-          </li>
-          <li class="navItem">
-            <a href="javascript:;">数码家电</a>
-          </li>
-          <li class="navItem">
-            <a href="javascript:;">严选全球</a>
-          </li>
-        </ul>
+		<div class="header">
+      <div class="headerSearch">
+        <a href="https://m.you.163.com/"></a>
+        <div class="searchBox">
+          <i class="icon-search"></i>
+          <span>搜索商品, 共25372款好物</span>
+        </div>
+        <button class="login">登录</button>
       </div>
-      <div class="toggle">
-        <i class="icon-toggle"></i>
+      <div class="headerNav">
+        <div class="navWrap" v-show="!isNavShow">
+          <div class="navLeft">
+            <ul class="navList">
+              <li class="navItem active">
+                <a href="javascript:;">推荐</a>
+              </li>
+              <li class="navItem">
+                <a href="javascript:;">居家生活</a>
+              </li>
+              <li class="navItem">
+                <a href="javascript:;">服饰鞋包</a>
+              </li>
+              <li class="navItem">
+                <a href="javascript:;">美食酒水</a>
+              </li>
+              <li class="navItem">
+                <a href="javascript:;">个护清洁</a>
+              </li>
+              <li class="navItem">
+                <a href="javascript:;">母婴亲子</a>
+              </li>
+              <li class="navItem">
+                <a href="javascript:;">运动旅行</a>
+              </li>
+              <li class="navItem">
+                <a href="javascript:;">数码家电</a>
+              </li>
+              <li class="navItem">
+                <a href="javascript:;">严选全球</a>
+              </li>
+            </ul>
+          </div>
+          
+        </div>
+        <div class="allNavWrap" v-show="isNavShow">
+          <p>全部频道</p>
+          <ul class="allNav">
+            <li class="item active">
+              <a href="javascript:;">推荐</a>
+            </li>
+            <li class="item">
+              <a href="javascript:;">居家生活</a>
+            </li>
+            <li class="item">
+              <a href="javascript:;">服饰鞋包</a>
+            </li>
+            <li class="item">
+              <a href="javascript:;">美食酒水</a>
+            </li>
+            <li class="item">
+              <a href="javascript:;">个护清洁</a>
+            </li>
+            <li class="item">
+              <a href="javascript:;">母婴亲子</a>
+            </li>
+            <li class="item">
+              <a href="javascript:;">运动旅行</a>
+            </li>
+            <li class="item">
+              <a href="javascript:;">数码家电</a>
+            </li>
+            <li class="item">
+              <a href="javascript:;">严选全球</a>
+            </li>
+          </ul>
+        </div>
+        <div class="toggle" @click="isNavShow=!isNavShow" :class="{active:isNavShow}">
+          <i class="icon-toggle"></i>
+        </div>
       </div>
     </div>
+    <div class="mask" v-show="isNavShow" @click="isNavShow=false"></div>
 	</div>
 </template>
 
 <script  type="text/ecmascript-6">
 	export default {
-
+    data(){
+      return {
+        isNavShow:false
+      }
+    }
 	};
 </script>
 
 <style lang='stylus' rel='stylesheet/stylus' scoped>
   #headerContainer
-    .headerSearch
-      padding 16px 30px
-      background-color #fff
-      display flex
-      align-items center
-      a
-        background-image url('https://yanxuan-static.nosdn.127.net/hxm/yanxuan-wap/p/20161201/style/img/icon-normal/indexLogo-a90bdaae6b.png')
-        width 138px
-        height 40px
-        margin-right 20px
-      .searchBox
-        width 442px
-        height 56px
-        background-color #ededed
-        border-radius 4px
+    height 148px
+    .header
+      position fixed 
+      left 0
+      top 0
+      z-index 2
+      .headerSearch
+        padding 16px 30px
+        background-color #fff
         display flex
         align-items center
-        justify-content center
-        .icon-search
-          background-image url('https://yanxuan-static.nosdn.127.net/hxm/yanxuan-wap/p/20161201/style/img/icon-normal/search2-553dba3aff.png?imageView&type=webp')
-          width 28px
-          height 28px
-          margin-right 10px
-          
-        span
-          color #666
-          font-size 28px
-      .login
-        width 74px
-        height 40px
-        border 1px solid red
-        border-radius 6px
-        background-color #fff
-        color red
-        font-size 24px
-        margin-left 15px
-    .headerNav
-      height 60px
-      background-color #fff
-      position relative
-      .inner
-        width 650px
-        overflow hidden
-        .navList
+        a
+          background-image url('https://yanxuan-static.nosdn.127.net/hxm/yanxuan-wap/p/20161201/style/img/icon-normal/indexLogo-a90bdaae6b.png')
+          width 138px
+          height 40px
+          margin-right 20px
+        .searchBox
+          width 442px
+          height 56px
+          background-color #ededed
+          border-radius 4px
           display flex
-          padding 0 30px
-          .navItem
-            box-sizing border-box
-            padding 0 16px
-            margin 0 10px
-            white-space nowrap
+          align-items center
+          justify-content center
+          .icon-search
+            background-image url('https://yanxuan-static.nosdn.127.net/hxm/yanxuan-wap/p/20161201/style/img/icon-normal/search2-553dba3aff.png?imageView&type=webp')
+            width 28px
+            height 28px
+            margin-right 10px
+            
+          span
+            color #666
+            font-size 28px
+        .login
+          width 74px
+          height 40px
+          border 1px solid red
+          border-radius 6px
+          background-color #fff
+          color red
+          font-size 24px
+          margin-left 15px
+      .headerNav
+        height 60px
+        background-color #fff
+        position relative
+        .navWrap
+          .navLeft
+            width 650px
+            overflow hidden
+            .navList
+              display flex
+              padding 0 30px
+              .navItem
+                box-sizing border-box
+                padding 0 16px
+                margin 0 10px
+                white-space nowrap
+                line-height 60px
+                text-align center
+                position relative
+                &.active
+                  &:after
+                    content ''
+                    display block
+                    position absolute
+                    left 0
+                    bottom 0
+                    width 100%
+                    height 4px
+                    background-color red
+                  a
+                    color red
+                a
+                  font-size 28px 
+                  color #333
+        .toggle
+          position absolute
+          top 0
+          right 0
+          z-index 2
+          width 100px 
+          height 100% 
+          display flex   
+          align-items center 
+          justify-content center
+          &.active
+            >.icon-toggle
+              transform rotate(180deg)
+          .icon-toggle
+            transition all .5s
+            width 30px
+            height 30px
+            background-image url('https://yanxuan-static.nosdn.127.net/hxm/yanxuan-wap/p/20161201/style/img/icon-normal/arrow-down-3-9b31adfa37.png?imageView&type=webp')
+          
+        .allNavWrap
+          display none
+          position absolute
+          left 0
+          top 0
+          display flex
+          flex-direction column
+          background-color #fff
+          
+          p
+            height 60px
             line-height 60px
-            text-align center
-            position relative
-            &.active
-              &:after
-                content ''
-                display block
-                position absolute
-                left 0
-                bottom 0
-                width 100%
-                height 4px
-                background-color red
+            color #333
+            font-size 28px
+            padding-left 30px
+          .allNav
+            padding-top 24px
+            display flex
+            flex-wrap wrap
+            .item
+              width 150px
+              height 56px
+              margin 0 0 40px 30px
+              text-align center
+              line-height 56px
+              border 1px solid #ccc
+              border-radius 8px
+              &.active
+                border 1px solid red
+                a
+                  color red
               a
-                color red
-            a
-              font-size 28px 
-              color #333
-      .toggle
-        position absolute
-        top 0
-        right 0
-        width 100px 
-        height 100% 
-        display flex   
-        align-items center 
-        justify-content center
-        .icon-toggle
-          width 30px
-          height 30px
-          background-image url('https://yanxuan-static.nosdn.127.net/hxm/yanxuan-wap/p/20161201/style/img/icon-normal/arrow-down-3-9b31adfa37.png?imageView&type=webp')
+                display block
+                width 100%
+                height 100%
+                color #333
+                font-size 24px
 
-
-
-
-
-
+    .mask
+      position fixed
+      left 0
+      top 0
+      right 0
+      bottom 0
+      z-index 1
+      background rgba(0,0,0,.5)
+      &.active
+        display none
 
 </style>
--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,19 +1,224 @@
 <template>
     <div id="homeContainer">
         <HeaderNav/>
+        <!-- 轮播图 -->
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide" v-for="(item,index) in imgsList" :key="index">
+									<img :src="item" alt="">
+								</div>
+            </div>
+						<!-- 如果需要分页器 -->
+						<div class="swiper-pagination"></div>
+        </div>
+				<div class="servicePolicy">
+					<ul class="list">
+						<li class="item">
+							<a href="javascript:;">
+								<img class="icon" src="https://yanxuan.nosdn.127.net/a03dd909803b9ac032eba58b7253a2f6.png"/>
+								<span class="text">网易自营品牌</span>
+							</a>
+						</li>
+						<li class="item">
+							<a href="javascript:;">
+								<img class="icon" src="https://yanxuan.nosdn.127.net/a03dd909803b9ac032eba58b7253a2f6.png"/>
+								<span class="text">30天无忧退货</span>
+							</a>
+						</li>
+						<li class="item">
+							<a href="javascript:;">
+								<img class="icon" src="https://yanxuan.nosdn.127.net/a03dd909803b9ac032eba58b7253a2f6.png"/>
+								<span class="text">48小时快速退款</span>
+							</a>
+						</li>
+					</ul>
+				</div>
+				<div class="navWrap">
+					<ul class="navList">
+						<li class="navItem">
+							<a href="javascript:;">
+								<img src="https://yanxuan.nosdn.127.net/c6fd8835a6400b7da7a016ad85506b69.png" alt="">
+								<span class="text">新品首发</span>
+							</a>
+						</li>
+						<li class="navItem">
+							<a href="javascript:;">
+								<img src="https://yanxuan.nosdn.127.net/c6fd8835a6400b7da7a016ad85506b69.png" alt="">
+								<span class="text">新品首发</span>
+							</a>
+						</li>
+						<li class="navItem">
+							<a href="javascript:;">
+								<img src="https://yanxuan.nosdn.127.net/c6fd8835a6400b7da7a016ad85506b69.png" alt="">
+								<span class="text">新品首发</span>
+							</a>
+						</li>
+						<li class="navItem">
+							<a href="javascript:;">
+								<img src="https://yanxuan.nosdn.127.net/c6fd8835a6400b7da7a016ad85506b69.png" alt="">
+								<span class="text">新品首发</span>
+							</a>
+						</li>
+						<li class="navItem">
+							<a href="javascript:;">
+								<img src="https://yanxuan.nosdn.127.net/c6fd8835a6400b7da7a016ad85506b69.png" alt="">
+								<span class="text">新品首发</span>
+							</a>
+						</li>
+						<li class="navItem">
+							<a href="javascript:;">
+								<img src="https://yanxuan.nosdn.127.net/c6fd8835a6400b7da7a016ad85506b69.png" alt="">
+								<span class="text">新品首发</span>
+							</a>
+						</li>
+						<li class="navItem">
+							<a href="javascript:;">
+								<img src="https://yanxuan.nosdn.127.net/c6fd8835a6400b7da7a016ad85506b69.png" alt="">
+								<span class="text">新品首发</span>
+							</a>
+						</li>
+						<li class="navItem">
+							<a href="javascript:;">
+								<img src="https://yanxuan.nosdn.127.net/c6fd8835a6400b7da7a016ad85506b69.png" alt="">
+								<span class="text">新品首发</span>
+							</a>
+						</li>
+						<li class="navItem">
+							<a href="javascript:;">
+								<img src="https://yanxuan.nosdn.127.net/c6fd8835a6400b7da7a016ad85506b69.png" alt="">
+								<span class="text">新品首发</span>
+							</a>
+						</li>
+						<li class="navItem">
+							<a href="javascript:;">
+								<img src="https://yanxuan.nosdn.127.net/c6fd8835a6400b7da7a016ad85506b69.png" alt="">
+								<span class="text">新品首发</span>
+							</a>
+						</li>
+					</ul>
+				</div>
+				<div class="bigPromotion">
+					<a href="javascript:;"></a>
+				</div>
     </div>
 </template>
 
 <script  type="text/ecmascript-6">
+    import Swiper from 'swiper'
+    import 'swiper/css/swiper.min.css'
     import HeaderNav from '../../components/HeaderNav/HeaderNav'
     export default {
+				data(){
+					return {
+						imgsList:[
+							'https://yanxuan.nosdn.127.net/84d82137e854e58bf26791db3ba203b8.jpg?type=webp&imageView&quality=75&thumbnail=750x0',
+							'https://yanxuan.nosdn.127.net/a16ac18c02bb26755dbcac1911631aa0.jpg?type=webp&imageView&quality=75&thumbnail=750x0',
+							'https://yanxuan.nosdn.127.net/bf2d7fc620ef5fc52c06bfff18a93024.jpg?type=webp&imageView&quality=75&thumbnail=750x0',
+							'https://yanxuan.nosdn.127.net/bec4a597a9aed55605eeb7c0c7710f9b.jpg?imageView&quality=75&thumbnail=750x0',
+							'https://yanxuan.nosdn.127.net/981c28d7f7276a2c84bf8c7790a9b559.jpg?imageView&quality=75&thumbnail=750x0',
+							'https://yanxuan.nosdn.127.net/feadfbdcc0e2dea285d92c6740d46c66.jpg?imageView&quality=75&thumbnail=750x0',
+							'https://yanxuan.nosdn.127.net/791dda3ae432212164b1668b25fbd11c.jpg?imageView&quality=75&thumbnail=750x0',
+							'https://yanxuan.nosdn.127.net/5afff79cb85dfcd465e6da0d8404ddae.jpg?imageView&quality=75&thumbnail=750x0'
+						]
+					}
+				},
         components:{
             HeaderNav
+        },
+        mounted(){
+          new Swiper('.swiper-container', {
+         		autoplay: true,//可选选项，自动滑动
+						loop:true,
+						pagination: {
+							el: '.swiper-pagination',
+							type: 'custom',
+							clickable:true,
+							renderCustom: function (swiper, current, total) {
+								var customPaginationHtml=''
+								for(let i=0;i<total;i++){
+									if(i===current-1){
+										customPaginationHtml+='<span class="swiper-pagination-customs swiper-pagination-customs-active"></span>'
+									}else{
+										customPaginationHtml+='<span class="swiper-pagination-customs"></span>'
+									}
+								}
+								return customPaginationHtml
+							}
+						}
+					})
         },
     };
 </script>
 
 <style lang='stylus' rel='stylesheet/stylus' scoped>
+	#homeContainer
+		.swiper-container
+			width 100%
+			height 296px
+			img
+				width 100%
+				height 100%
+			/deep/.swiper-pagination-customs
+				width 40px
+				height 4px
+				background-color #fff
+				opacity .4
+				display inline-block
+				margin-right 10px
+			/deep/.swiper-pagination-customs-active
+				opacity 1
+		.servicePolicy
+			height 72px
+			padding 0 30px	
+			background-color #fff
+			.list
+				height 100%
+				display flex
+				align-items center
+				.item
+					width 278px
+					height 36px 
+					a
+						display inline-block
+						height 100%
+						.icon
+							width 32px
+							height 32px
+							vertical-align middle	
+						.text
+							vertical-align middle
+							font-size 24px
+							color #333
+							margin-left 8px
 
- 
+		.navWrap
+			width 100%
+			padding-bottom 32px
+			.navList
+				width 100%
+				height 340px
+				display flex
+				flex-wrap wrap
+				.navItem
+					a
+						display flex
+						flex-direction column
+						width 110px
+						height 156px
+						margin 9px 20px 
+						img
+							width 110px
+							height 110px
+						.text
+							font-size 24px
+							color #333
+							margin-top 9px
+							text-align center
+		.bigPromotion
+			a
+				display block
+				width 100%
+				height 240px
+				background-image url('https://yanxuan.nosdn.127.net/7db5ad89be165b82b6a3e261029d0122.gif?imageView&quality=75')
+				background-size 100% 100%
 </style>
