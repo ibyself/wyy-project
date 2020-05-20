40d1e0b2f1132c36d3ca8d0b8336b83e3f64f2f1
sun <chentao980225@163.com>
2020-05-20 23:04:48 +0800

    update

--- src/pages/Topic/Waterfall/Waterfall.vue
+++ src/pages/Topic/Waterfall/Waterfall.vue
@@ -1,12 +1,12 @@
 <template>
     <div id="waterfall_box">
         <ul class="waterfall_cont">
-            <!-- <li class="waterItem" v-for="(waterItem,index) in waterfallData" :key="index">
+            <li class="waterItem" v-for="(waterItem,index) in waterfallData" :key="index">
                 <ShowBox :topics1="waterItem.topics"/>
-            </li> -->
-            <li class="waterItem" v-for="(recItem,index) in recAutoData" :key="index+Date.now()">
-                <ShowBox :topics2="recItem.topics"/>
             </li>
+            <!-- <li class="waterItem" v-for="(recItem,index) in recAutoData" :key="index+Date.now()">
+                <ShowBox :topics2="recItem.topics"/>
+            </li> -->
         </ul>
     </div>
 </template>
@@ -35,15 +35,11 @@
         justify-content center
         .waterfall_cont
             width 710px
-            display flex
-            flex-direction row
-            flex-wrap wrap
-            justify-content space-between
-            align-items flex-start
+            column-count 2
+            column-gap 26px
+            min-height 100vh
             .waterItem
                 width 342px
-                display flex
-                flex-direction column
                 background-color white
                 border-radius .24rem
                 margin-bottom 26px
